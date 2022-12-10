f39289890fdbfa7f98d5d1ee17db5adf68a71e8f
Chuliaen <46786325+Chuliaen@users.noreply.github.com>
2022-12-10 19:08:19 +0100

    t

---
+++ src/main/java/org/unotmp/game/GameCreator.java
@@ -0,0 +1,40 @@
+package org.unotmp.game;
+
+import org.unotmp.card.CardDeckGenerator;
+import org.unotmp.player.PlayerCardList;
+import org.unotmp.player.Player;
+
+public class GameCreator {
+
+    private String[] playerNames = new String[10];
+    private int currentPlayers = 0;
+
+    public GameCreator(String playerName) {
+        playerNames[0] = playerName;
+        currentPlayers = 1;
+    }
+
+    public PlayerIterator start(DrawPile drawPile) {
+        Player[] players = new Player[currentPlayers];
+
+        for (int i = 0; i < currentPlayers; i++) {
+            PlayerCardList cards = new PlayerCardList();
+            for (int j = 0; j < 7; j++) {
+                cards.addCard(drawPile.drawCard());
+            }
+
+            players[i] = new Player(playerNames[i], cards);
+        }
+
+        return new PlayerIterator(players);
+    }
+
+    public boolean join(String playerName) {
+        if (currentPlayers < 10) {
+            playerNames[currentPlayers] = playerName;
+            currentPlayers += 1;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/unotmp/game/PlayerIterator.java
@@ -0,0 +1,37 @@
+package org.unotmp.game;
+
+import org.unotmp.player.Player;
+
+public class PlayerIterator {
+
+    private final Player[] players;
+    private int current = 0;
+    private GameDirection gameDirection = GameDirection.CLOCKWISE;
+
+    public PlayerIterator(Player[] players) {
+        this.players = players;
+    }
+
+    public Player getCurrentPlayer() {
+        return players[current];
+    }
+
+    public void changeDirection() {
+        if (gameDirection == GameDirection.CLOCKWISE) {
+            gameDirection = GameDirection.COUNTER_CLOCKWISE;
+        } else {
+            gameDirection = GameDirection.CLOCKWISE;
+        }
+    }
+
+    public Player getNextPlayer() {
+        if (gameDirection == GameDirection.CLOCKWISE) {
+            current = (current + 1) % players.length;
+        } else {
+            current = (current - 1 + players.length) % players.length;
+        }
+        return getCurrentPlayer();
+    }
+
+    //todo: remove player
+}
